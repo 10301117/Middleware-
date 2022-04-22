11db068f395a99e41457df384bfaabf6d2b7f635
10301117 <101034126+10301117@users.noreply.github.com>
2022-04-22 19:50:21 +0800

    Update StudentDAOIMP.java

--- StudentDAOIMP.java
+++ StudentDAOIMP.java
@@ -6,24 +6,24 @@ import java.sql.*;
 import connection.DataBaseConnection;
 
 public class StudentDAOIMP implements StudentDAO{
-// 添加操作
-     public void insert(Student s){
-     String sql = "INSERT INTO student (id, name) values (?,?)";
-     PreparedStatement pstmt = null;
-     DataBaseConnection conn = null;
-	    //针对数据库的具体操作
-         try{
-             conn = new DataBaseConnection();
-	        
-             pstmt = conn.getConnection().prepareStatement(sql);
-             pstmt.setLong(1,s.getID());
-             //pstmt.setString(1,s.getID());
-             pstmt.setString(2,s.getName());
-	  
-             pstmt.executeUpdate();
-             pstmt.close();
-             conn.close();
-         }catch(Exception e){  }
+	
+    public void insert(Student s){
+        String sql = "INSERT INTO student (id, name) values (?,?)";
+        PreparedStatement pstmt = null;
+        DataBaseConnection conn = null;
+        //针对数据库的具体操作
+        try{
+            conn = new DataBaseConnection();
+
+            pstmt = conn.getConnection().prepareStatement(sql);
+            pstmt.setLong(1,s.getID());
+            //pstmt.setString(1,s.getID());
+            pstmt.setString(2,s.getName());
+
+            pstmt.executeUpdate();
+            pstmt.close();
+            conn.close();
+        }catch(Exception e){ }
     }
 
     public void update(Student s) {
@@ -32,34 +32,33 @@ public class StudentDAOIMP implements StudentDAO{
         DataBaseConnection conn = null;
         try{
             conn = new DataBaseConnection();
-            
+
             pstmt = conn.getConnection().prepareStatement(sql);
             pstmt.setLong(1,s.getName());
             pstmt.setString(2,s.getID());
-            
+
             pstmt.executeUpdate();
             pstmt.close();
             conn.close();
-        }
+        }catch(Exception e){ }
     }
-    
+
     public void delete(String iD) {
         String sql = "delete from Student where ID=?";
         PreparedStatement pstmt = null;
         DataBaseConnection conn = null;
         try{
             conn = new DataBaseConnection();
-            
+
             pstmt = conn.getConnection().prepareStatement(sql);
             pstmt.setString(1,s.getID());
-            
+
             pstmt.executeUpdate();
             pstmt.close();
             conn.close();
-        }
-        catch(Exception e){}
+        }catch(Exception e){ }
     }
-    
+
     public List<Student> findAll() {
         String sql = "select * from Student where ID=?";
         List<Student> list = null;
@@ -68,10 +67,10 @@ public class StudentDAOIMP implements StudentDAO{
         ResultSet rs = null;
         try{
             conn = new DataBaseConnection();
-            
+
             pstmt = conn.getConnection().prepareStatement(sql);
             pstmt.setString(1,s.getID());
-            
+
             rs = pstmt.executeQuery();
             Student s = new Student();
             while(rs.next()) {
@@ -82,10 +81,10 @@ public class StudentDAOIMP implements StudentDAO{
             pstmt.close();
             conn.close();
             rs.close();
-        } catch(Exception e){}
+        } catch(Exception e){ }
         return list;
     }
-    
+
     public Student findByID(Long id) {
         String sql = "select * from Student where ID=?";
         List<Student> list = null;
@@ -94,11 +93,11 @@ public class StudentDAOIMP implements StudentDAO{
         ResultSet rs = null;
         try{
             conn = new DataBaseConnection();
-            
+
             pstmt = conn.getConnection().prepareStatement(sql);
             pstmt.setString(1,s.getID());
             pstmt.setString(2,id);
-            
+
             rs = pstmt.executeQuery();
             Student s = new Student();
             while(rs.next()) {
@@ -109,7 +108,7 @@ public class StudentDAOIMP implements StudentDAO{
             pstmt.close();
             conn.close();
             rs.close();
-        } catch(Exception e){}
+        } catch(Exception e){ } 
         return list.get(0);
     }
 
